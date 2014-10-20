98dd863c0927b8e3f8f38b745ef9cb0e9f6baf12
bschwyn <ben.schwyn@gmail.com>
2014-10-19 22:31:27 -0700

    working on draw_node method

--- Application.py
+++ Application.py
@@ -84,11 +84,14 @@ class Application(Tk.Frame):
         nodebox = nbox.NodeBox(self,self)
         #self.wait_window(inputDialog.top)
         #self.dimensions = inputDialog.dimensions ###not ture anymore
-        
+    
+    
+    #adds the information about the node recieved from the nodebox the model
     def add_node_to_model(self,source,x,y,length=1.0,strain=1.0):
         self.model.add_node_to(source,x,y,length,strain)  
-    
         
+        
+    #I'm pretty sure this needs to be discarded    
     def model_widgets(self):
         self.new_node_l = Tk.Label(root, self)
         self.new_node_l['text'] = 'new node information'
@@ -98,7 +101,27 @@ class Application(Tk.Frame):
         
         self.node_button = Tk.Button(self)
         self.node_button["text"] = 'enter new info'     
-      
+
+    def draw_node(self, source, x, y, length, strain):
+        radius = .5 #some number
+        bounds = 2**.5 * radius
+        #bounding box = 1.414*radius
+        model = self.model
+        if source is None:
+            origin = self.frame.create_circle(x - bounds, y - bounds, x + bounds, x + bounds)
+        else:
+            model
+            new_node = self.frame.create_circle(x-bounds, y-bounds, x + bounds, x + bounds)
+            source_x = model.getNodeAttributes(source,"x")
+            source_y = model.getNodeAttributes(source, "y")
+            new_line = self.frame.create_line(source_x,source_y, x ,y)
+        
+            #draw circle at coordinates w/ radius
+        #else
+        #draw circle at coordinates
+        #draw line from coordinates of source_node to new coordinates
+        #remove circle at source node, replace with dot
+        
         
         #create a new set of widgets explicitely for adding and deleting new nodes, and accessing that information.
     
