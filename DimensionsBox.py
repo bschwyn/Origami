22018d583c84dbc09b20b7c81fc9a687200ecc2f
bschwyn <ben.schwyn@gmail.com>
2014-10-28 18:59:02 -0700

    added a some title stuff

--- DimensionsBox.py
+++ DimensionsBox.py
@@ -5,10 +5,18 @@ class DimSubmissionBox(object):
     def __init__(self,parent):
         
         top = self.top = Tk.Toplevel(parent)
+        top.wm_title("Initalize Model")
 
         
         dialog = Tk.Frame(self.top)
         dialog.grid()
+        
+        self.l_name = Tk.Label(dialog)
+        self.l_name["text"] = "Name:"
+        self.l_name.grid(row = 0, column = 0)
+        
+        self.e_name = Tk.Entry(dialog)
+        self.e_name.grid(row = 0, column = 1)
         
         #height and width labels
         self.l_height = Tk.Label(dialog)
@@ -16,31 +24,33 @@ class DimSubmissionBox(object):
         self.l_width = Tk.Label(dialog)
         self.l_width["text"] = "Width:"
             
-        self.l_height.grid(row = 0)
-        self.l_width.grid(row = 1)
+        self.l_height.grid(row = 1)
+        self.l_width.grid(row = 2)
         
         #height and width entry boxes
         self.e_height = Tk.Entry(dialog)
         self.e_width = Tk.Entry(dialog)
-        self.e_height.grid(row = 0, column = 1)
-        self.e_width.grid(row = 1, column = 1)
+        self.e_height.grid(row = 1, column = 1)
+        self.e_width.grid(row =0, column = 1)
         
         #submits data
         self.b_submit = Tk.Button(dialog)
         self.b_submit["text"] = "Submit"
         self.b_submit["command"] = lambda: self.submit_data()
-        self.b_submit.grid(row =2, columnspan = 2)
+        self.b_submit.grid(row =3, columnspan = 2)
 
         #cancel button    
         self.b_cancel = Tk.Button(dialog)
         self.b_cancel["text"] = "Cancel"
         self.b_cancel["command"] = self.top.destroy
-        self.b_cancel.grid(row = 3,columnspan = 2)
+        self.b_cancel.grid(row = 4,columnspan = 2)
                         
     def submit_data(self):
         height_data = self.e_height.get()
         width_data = self.e_width.get()
         data = (width_data, height_data)
+        
+        name = self.e_name.get()
         if data:
             self.entered_dimensions = data
             self.top.destroy()
