004bdbbe00f78f634b012dda51778641fd13456d
bschwyn <ben.schwyn@gmail.com>
2014-10-29 20:45:59 -0700

    Draw class works

--- Application.py
+++ Application.py
@@ -233,7 +233,7 @@ class Application(Tk.Frame):
             
             self.frame.create_text(x_halfway, y_halfway, text = "Length = " + str(length) + "\n"+"Strain = " + str(strain))
             
-            
+          
         
         
         #create a new set of widgets explicitely for adding and deleting new nodes, and accessing that information.
@@ -245,7 +245,7 @@ class Application(Tk.Frame):
         new_x_vector = optimize_return_objects.x
         new_scale = new_x_vector[-1]
 
-class Draw(Application):
+class Draw:
     def __init__(self,application, current_node, source, x, y, length, strain):
         self.current = current_node
         self.source = source
@@ -253,14 +253,14 @@ class Draw(Application):
         self.y = y
         self.length = length
         self.strain = strain
-        self.draw_node(self.current, self.source, self.x, self.y, self.length, self.strain)
+        self.draw_node(application, self.current, self.source, self.x, self.y, self.length, self.strain)
     
 
-    def draw_node(self, current_node, source, x, y, length, strain):
+    def draw_node(self, application, current_node, source, x, y, length, strain):
     
         #width of square in pixels
         paper_long_edge_pixels = application.frame_pixels - 2 * application.border_pixels
-        radius = self.frame_pixels/10 #some number
+        radius = application.frame_pixels/10 #some number
         
             
         #get paper size
@@ -280,8 +280,8 @@ class Draw(Application):
         y_pixels = paper_height_pixels * y / paper_height   
         
         #distance in pixels from corner of frame to node
-        x_corner_dist = self.border_pixels + x_pixels
-        y_corner_dist = self.border_pixels + y_pixels
+        x_corner_dist = application.border_pixels + x_pixels
+        y_corner_dist = application.border_pixels + y_pixels
         
         #size of rectangle at node location
         dot = 2.5
@@ -294,38 +294,38 @@ class Draw(Application):
         
         #draw a circle
         rad = length * radius
-        self.circle = self.frame.create_oval(x_corner_dist - rad ,y_corner_dist - rad, x_corner_dist + rad, y_corner_dist + rad) 
+        self.circle = application.frame.create_oval(x_corner_dist - rad ,y_corner_dist - rad, x_corner_dist + rad, y_corner_dist + rad) 
         
         #node number and coordinates label 
-        self.node_label = self.frame.create_text(x_corner_dist + 10, y_corner_dist, text = "node" + str(current_node) + "\n" + "(" + str(x) + "," + str(y) + ")")  
+        self.node_label = application.frame.create_text(x_corner_dist + 10, y_corner_dist, text = "node" + str(current_node) + "\n" + "(" + str(x) + "," + str(y) + ")")  
         
         
         if source is None:
             #origin_circle = self.frame.create_oval(x - bounds, y - bounds, x + bounds, x + bounds)
-            b = self.border_pixels
+            b = application.border_pixels
             
-            self.dot = self.frame.create_rectangle(x_corner_dist - dot, y_corner_dist - dot, x_corner_dist + dot, y_corner_dist + dot, fill = "red")
+            self.dot = application.frame.create_rectangle(x_corner_dist - dot, y_corner_dist - dot, x_corner_dist + dot, y_corner_dist + dot, fill = "red")
             
         #draw a  line
         else:
             #draw rectangle dot at node location
-            self.dot = self.frame.create_rectangle(x_corner_dist - dot, y_corner_dist - dot, x_corner_dist + dot, y_corner_dist + dot, fill = "black")
+            self.dot = application.frame.create_rectangle(x_corner_dist - dot, y_corner_dist - dot, x_corner_dist + dot, y_corner_dist + dot, fill = "black")
            
             #draw line between nodes
-            source_x = self.model.getNodeAttribute(source,"x")
-            source_y = self.model.getNodeAttribute(source, "y")
+            source_x = application.model.getNodeAttribute(source,"x")
+            source_y = application.model.getNodeAttribute(source, "y")
             
-            x_s_corner_dist = self.border_pixels + source_x * paper_width_pixels / paper_width
-            y_s_corner_dist = self.border_pixels + source_y * paper_height_pixels / paper_height
+            x_s_corner_dist = application.border_pixels + source_x * paper_width_pixels / paper_width
+            y_s_corner_dist = application.border_pixels + source_y * paper_height_pixels / paper_height
             
-            self.new_line = self.frame.create_line(x_s_corner_dist, y_s_corner_dist, x_corner_dist, y_corner_dist)
+            self.new_line = application.frame.create_line(x_s_corner_dist, y_s_corner_dist, x_corner_dist, y_corner_dist)
             
             
             #draw label halfway for length and strain
-            x_halfway = self.border_pixels + (source_x + x) / 2 * paper_width_pixels / paper_width
-            y_halfway = self.border_pixels + (source_y + y) / 2 * paper_height_pixels / paper_height
+            x_halfway = application.border_pixels + (source_x + x) / 2 * paper_width_pixels / paper_width
+            y_halfway = application.border_pixels + (source_y + y) / 2 * paper_height_pixels / paper_height
             
-            self.edge_label = self.frame.create_text(x_halfway, y_halfway, text = "Length = " + str(length) + "\n"+"Strain = " + str(strain))
+            self.edge_label = application.frame.create_text(x_halfway, y_halfway, text = "Length = " + str(length) + "\n"+"Strain = " + str(strain))
 
     def undo(self):
         self.delete(circle)
